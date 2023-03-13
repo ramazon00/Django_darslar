483738d981676e1ca3041818b9492b904f91d9f5
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-03-13 14:31:10 +0500

    13_dars
    Notes

---
+++ notes/document/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Meta:
+    ordering = ('title',)
---
+++ notes/notes/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from document.views import editor, delete_document
+
+urlpatterns = [
+    path('', editor, name='editor'),
+    path('delete_document/<int:docid>/', delete_document, name='delete_document'),
+    path('admin/', admin.site.urls),
+]
