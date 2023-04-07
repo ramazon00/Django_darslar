1e553d9c3e445740aef765d0920fe29919cca272
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-04-07 14:51:51 +0500

    9_dars
    accounts/views.py

--- views.py
+++ views.py
@@ -1,27 +1,7 @@
-from django.views.generic import ListView, DetailView, TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Yangilik
-from django.urls import reverse_lazy
+from django.shortcuts import render
+from articles.models import Article
+from django.views.generic import ListView, DetailView
 
-class BlogListView(ListView):
-    model = Yangilik
+class home(ListView):
+    model  = Article
     template_name = 'home.html'
-
-class BlogDetailView(DetailView):
-    model = Yangilik
-    template_name = 'post_detail.html'
-
-class BlogCreateView(CreateView):
-    model = Yangilik
-    template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
-
-class BlogUpdateView(UpdateView):
-    model = Yangilik
-    template_name = 'post_edit.html'
-    fields = ['title', 'body']
-
-class BlogDeleteView(DeleteView):
-    model = Yangilik
-    template_name = 'post_delete.html'
-    succes_url = reverse_lazy('home')
